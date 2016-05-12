11aa2cd7b9054cf3ff73f8d5c8156b7833a8c76a
Jeff Pittman <jeff.pittman@gmail.com>
2016-05-12 10:47:41 -0700

    Update HelloWorldC.c

--- HelloWorldC.c
+++ HelloWorldC.c
@@ -1,9 +1,9 @@
 // First C Program - Outputs Hello World
 // Created from The C Programming Language Book, 2nd Edition
 
-#include <stdio.h>
+#include <stdio.h> //include standard library
 
-int main()
-  {
-    printf("Hello, World, for the second time\n");
+int main() //definate MAIN function, no args
+  { 
+    printf("Hello, World, for the second time\n"); //call printf from stdio library, \n represents new line
   }
